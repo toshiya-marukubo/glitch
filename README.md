47e059fdee3a3ab87d5de70a32fffaaa92bc1371
ToshiyaMarukubo <marukubo.toshiya@gmail.com>
2021-11-26 23:17:11 +0900

    create script files.

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# glitch
+# Glitch
+It is be able to add glitch effect to canvas.
+## How to use
+It is easy to use it.
+- Parameters
+  - ctx : canvas context
+  - width : canvas width
+  - height : canvas height
+  - min : min split number
+  - max : max split number
+  - t : time or frame
+```
+// load script file
+<script src="./glitch-prototype-version.js"></script>
+
+// on main program
+function init() {
+  ...
+  ...
+  ...
+
+  glitch = new Glitch(ctx, width, height, min, max);
+  
+  ...
+  ...
+  ...
+}
+
+function draw() {
+  ...
+  ...
+  ...
+
+  glitch.render(t);  // t is time or frame
+
+  requestAnimationFrame(draw);
+}
+```
+### License
+MIT
---
+++ glitch-class-version.js
@@ -0,0 +1,67 @@
+/**
+ * @class - glitch class
+ * @params {number} width - canvas width
+ * @params {number} height - canvas height
+ * @params {number} min - split min number
+ * @params {number} max - split max number
+ */
+class Glitch {
+  constructor(ctx, width, height, min, max) {
+    this.ctx = ctx;
+    this.width = width;
+    this.height = height;
+    this.min = min;
+    this.max = max;
+    this.imageArr = [];
+    this.heightArr = [];
+  }
+
+  /**
+   * @params {number} t - time
+   */
+  getImageData(t) {
+    let preHeight = 0;
+    let addHeight = 0;
+    for (let i = 0; i < this.height; i += addHeight) {
+      addHeight = Math.floor((this.max - this.min) * Math.random() + this.min);
+
+      let image = this.ctx.getImageData(0, preHeight, this.width, preHeight + addHeight);
+
+      this.dataArr.push(image);
+      this.heightArr.push(preHeight);
+
+      preHeight += addHeight
+    }
+  }
+
+  /**
+   * @params {number} t - time
+   */
+  addImage(t) {
+    for (let i = 0; i < this.dataArr.length ; i++) {
+      if (Math.random() > 0.01) {
+        this.ctx.putImageData(
+          this.dataArr[i],
+          Math.sin(this.heightArr[i] * 0.1 + t) * 3 * Math.random(),
+          this.heightArr[i]
+        );
+      } else {
+        this.ctx.putImageData(
+          this.dataArr[Math.floor(this.dataArr.length * Math.random())],
+          this.width * Math.random() - this.width / 2,
+          this.heightArr[i]
+        );
+      }
+    }
+  }
+
+  /**
+   * @params {number} t - time
+   */
+  render(t) {
+    this.dataArr = [];
+    this.heightArr = [];
+    this.getImageData(t);
+    this.addImage(t);
+  }
+}
